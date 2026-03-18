260676b19222211ab487e911ef62a27f37004097
kirrer-izo <isaackmuchoki55@gmail.com>
2026-03-18 15:41:41 +0300

    add debug line to see backend contents

--- start.sh
+++ start.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 echo "=== Root contents ==="
 ls /var/www/html/
+echo "=== Backend contents ==="
+ls /var/www/html/backend/
 echo "=== Checking file exists ==="
 ls /var/www/html/backend/Infrastructure/database/
 echo "=== Running migrations ==="
