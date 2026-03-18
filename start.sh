62587d38bc1d769df832449f1fd6d9b620dfbedb
kirrer-izo <isaackmuchoki55@gmail.com>
2026-03-18 15:31:36 +0300

    chore: add debug logs to start.sh and verify migrations

--- start.sh
+++ start.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
+echo "=== Checking file exists ==="
+ls /var/www/html/backend/Infrastructure/database/
+echo "=== Running migrations ==="
 php /var/www/html/backend/Infrastructure/database/migrate.php
+echo "=== Starting Apache ==="
 apache2-foreground
